a2609ae404c86ed79b048ee0679389c945b7bd76
SAMEER TARIQ <imsamtar@gmail.com>
2020-06-24 21:32:43 +0500

    Project structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,24 @@
 <script>
-	export let name;
+  import { Pages, Page } from "svelte-route";
+
+  import Navbar from "./components/Navbar.svelte";
+  import NotFound from "./pages/NotFound.svelte";
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
+  import Settings from "./pages/Settings.svelte";
+  import Item from "./pages/Item.svelte";
+  import Items from "./pages/Items.svelte";
+  import Diagram from "./pages/Diagram.svelte";
+  import Editor from "./pages/Editor.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Pages alt={NotFound}>
+  <Navbar />
+  <Page route="/" src={Home} />
+  <Page route="/login" src={Login} />
+  <Page route="/settings" src={Settings} />
+  <Page route="/item" src={Item} />
+  <Page route="/items" src={Items} />
+  <Page route="/diagram" src={Diagram} />
+  <Page route="/editor" src={Editor} />
+</Pages>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,35 @@
+<style>
+  nav {
+    background: #333;
+  }
+  .container {
+    margin: 0 auto;
+    height: 60px;
+    display: flex;
+  }
+  .container {
+    max-width: 1300px;
+  }
+  .container > a {
+    color: white;
+    text-decoration: none;
+    display: flex;
+    padding: 0 1rem;
+    align-items: center;
+    justify-content: center;
+  }
+  .container > a:hover {
+    background: #ffffff22;
+  }
+  div.spacer {
+    width: 100%;
+  }
+</style>
+
+<nav>
+  <div class="container">
+    <a href="/">Home</a>
+    <div class="spacer">.</div>
+    <a href="/login">Login</a>
+  </div>
+</nav>
