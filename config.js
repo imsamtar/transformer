c3183706d57167fe9d170f3f76b78ba1672c8944
SAMEER TARIQ <imsamtar@gmail.com>
2020-07-04 21:17:15 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+export default {
+    firebaseConfig: {
+        apiKey: "AIzaSyBm8xVHDFuEMcC_eg6o5WMQOtJ3lItIawA",
+        authDomain: "fruit-price-tracker.firebaseapp.com",
+        databaseURL: "https://fruit-price-tracker.firebaseio.com",
+        projectId: "fruit-price-tracker",
+        storageBucket: "fruit-price-tracker.appspot.com",
+        messagingSenderId: "560833857160",
+        appId: "1:560833857160:web:a7770c22b8bca68673413d",
+        measurementId: "G-K888NRNFPX"
+    }
+}
